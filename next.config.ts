0d6daec565335aa4ef031cf018f4452674845ac0
Royal1king <theroyalking112233@gmail.com>
2025-07-04 17:24:29 +0530

    deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com',"127.0.0.1", "localhost",'ecommercewebsiteapi-production.up.railway.app'],
+    domains: ['lh3.googleusercontent.com',"127.0.0.1", "localhost",'ecommercewebsiteapi-production.up.railway.app','ecommerce-website-api-zvg2.onrender.com','res.cloudinary.com','res.cloudinary.com',],
   },
 };
 
